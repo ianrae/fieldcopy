12f4ac4ceeecc1457b4112ee0e2ba03a381da085
ianrae <ianrae88@gmail.com>
2019-03-02 18:15:36 -0500

    refac

---
+++ src/main/java/org/dnal/fc/beanutils/ExecuteCopySpec.java
@@ -0,0 +1,8 @@
+package org.dnal.fc.beanutils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExecuteCopySpec {
+	public List<ExecuteFieldSpec> fieldL = new ArrayList<>();
+}
---
+++ src/main/java/org/dnal/fc/beanutils/ExecuteFieldSpec.java
@@ -0,0 +1,11 @@
+package org.dnal.fc.beanutils;
+
+import org.dnal.fc.FieldCopyMapping;
+import org.dnal.fc.core.FieldPair;
+import org.dnal.fc.core.ValueTransformer;
+
+public class ExecuteFieldSpec {
+	public FieldPair pair;
+	public ValueTransformer transformer;
+	public FieldCopyMapping mapping;
+}
