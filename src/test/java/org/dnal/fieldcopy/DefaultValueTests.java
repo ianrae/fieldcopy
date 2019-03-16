0cbfd530adc1b9a2e1f698435e1677b3ac902df1
ianrae <ianrae88@gmail.com>
2019-03-16 17:38:02 -0400

    works

--- src/test/java/org/dnal/fieldcopy/DefaultValueTests.java
+++ src/test/java/org/dnal/fieldcopy/DefaultValueTests.java
@@ -2,27 +2,91 @@ package org.dnal.fieldcopy;
 
 import static org.junit.Assert.assertEquals;
 
-import org.dnal.fieldcopy.FieldCopierTests.Dest;
-import org.dnal.fieldcopy.FieldCopierTests.Source;
 import org.junit.Test;
 
 public class DefaultValueTests extends BaseTest {
 	
+	public static class Source {
+		private String name;
+		private String title;
+
+		public Source() {
+		}
+		public Source(String name, String title) {
+			this.name = name;
+			this.title = title;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+	}
+	public static class Dest {
+		private String name;
+		private String title;
+
+		public Dest() {
+		}
+		public Dest(String name, String title) {
+			this.name = name;
+			this.title = title;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+	}
+	
 	
 	@Test
-	public void testCopyByName() {
-		Source src = new Source("bob", 33);
-		Dest dest = new Dest(null, -1);
+	public void test() {
+		Source src = new Source(null, null);
+		Dest dest = new Dest(null, null);
 		
 		FieldCopier copier = createCopier();
-		copier.copy(src, dest).cacheKey("key1").field("name", "name").execute();
-		assertEquals("bob", dest.getName());
-		assertEquals(-1, dest.getAge());
-		
-		dest = new Dest(null, -1);
-		copier.copy(src, dest).cacheKey("key2").field("name", "name", "sam").execute();
+		copier.copy(src, dest).cacheKey("key1").field("name", "name").field("title", "title").execute();
 		assertEquals(null, dest.getName());
-		assertEquals(33, dest.getAge());
+		assertEquals(null, dest.getTitle());
+		
+		dest = new Dest(null, null);
+		copier.copy(src, dest).cacheKey("key2").field("name", "name", "sam").field("title", "title", "t2").execute();
+		assertEquals("sam", dest.getName());
+		assertEquals("t2", dest.getTitle());
+	}
+	@Test
+	public void testWhenNotUsed() {
+		Source src = new Source("bob", "t1");
+		Dest dest = new Dest(null, null);
+		
+		FieldCopier copier = createCopier();
+		copier.copy(src, dest).cacheKey("key1").field("name", "name").field("title", "title").execute();
+		assertEquals("bob", dest.getName());
+		assertEquals("t1", dest.getTitle());
+		
+		dest = new Dest(null, null);
+		copier.copy(src, dest).cacheKey("key2").field("name", "name", "sam").field("title", "title", "t2").execute();
+		assertEquals("bob", dest.getName());
+		assertEquals("t1", dest.getTitle());
 	}
 	
 	//--
