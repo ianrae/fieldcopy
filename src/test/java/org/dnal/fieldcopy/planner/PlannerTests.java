e47b9ee6c97eb0649c01a541cc252b188f795b79
ianrae <ianrae88@gmail.com>
2019-03-30 10:28:45 -0400

    m

--- src/test/java/org/dnal/fieldcopy/planner/PlannerTests.java
+++ src/test/java/org/dnal/fieldcopy/planner/PlannerTests.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.beans.PropertyDescriptor;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
@@ -156,53 +157,143 @@ public class PlannerTests extends BaseTest {
 //				throw new FieldCopyException(error);
 //			}
 			
-			
-			for(FieldPair pair: copySpec.fieldPairs) {
-	            final FieldDescriptor origDescriptor = pair.srcProp;
-	            final String name = origDescriptor.getName();
-	            
-	            if (propertyUtils.isReadable(sourceObj, name) &&
-	            		propertyUtils.isWriteable(destObj, pair.destFieldName)) {
-	            	try {
-	            		fillInDestPropIfNeeded(pair, destObj.getClass());
-	            		
-	            		BeanUtilsFieldDescriptor fd1 = (BeanUtilsFieldDescriptor) pair.srcProp;
-	            		Class<?> srcType = fd1.pd.getPropertyType();
-	            		
-	            		if (beanDetectorSvc.isBeanClass(srcType)) {
-	            			
-	            		} else {
-	            			
-	            		}
-	            		
-//	            		
-//	            		converterSvc.addListConverterIfNeeded(pair, copySpec, destObj.getClass());
-//	            		converterSvc.addArrayListConverterIfNeeded(pair, copySpec, destObj.getClass());
-//	            		converterSvc.addArrayConverterIfNeeded(pair, copySpec, destObj.getClass());
-//	            		converterSvc.addListArrayConverterIfNeeded(pair, copySpec, destObj.getClass());
-	            		
-//	            		//a mapping is an explicit set of instructions for copying sub-objects (i.e. sub-beans)
-//	            		FieldCopyMapping mapping = findOrGenerateMapping(pair, mappingL, outerSvc, copySpec);
-//	            		if (mapping != null) {
-//	            			FieldPlan fspec = new FieldPlan();
-//	            			fspec.pair = pair;
-//	            			fspec.mapping = mapping;
-//	            			execspec.fieldL.add(fspec);
-//	            		} else {
-//	            			validateIsAllowed(pair);
-//	            			
-//	            			FieldPlan fspec = new FieldPlan();
-//	            			fspec.pair = pair;
-//	            			fspec.converter = converterSvc.findConverter(copySpec, pair, orig, copySpec.converterL);
-//	            			execspec.fieldL.add(fspec);
-//	            		}
-	            		
-	            	} catch (final Exception e) { //was NoSuchMethodException
-	            		// Should not happen
-	            	}
-	            }
+			try {
+				ZClassPlan classPlan = this.buildClassPlan(sourceObj, destObj, sourceObj.getClass(), destObj.getClass(), copySpec.fieldPairs);
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
 		}
+		
+		private void validateIsAllowed(FieldPair pair) throws NoSuchMethodException, InstantiationException, IllegalAccessException {
+			Class<?> destClass = isNotAllowed(pair);
+			if (destClass != null) {
+				BeanUtilsFieldDescriptor fd1 = (BeanUtilsFieldDescriptor) pair.srcProp;
+				String err = String.format("Not allowed to copy %s to %s", fd1.pd.getPropertyType().getName(), destClass.getName());
+				throw new FieldCopyException(err);
+			}
+		}
+
+		/**
+		 * Additional rules that we want to enfore.
+		 * For example, not allowed to copy an int to a boolean.
+		 * 
+		 * @return null if allowed, else class that we are NOT allowed to copy to
+		 */
+		private Class<?> isNotAllowed(FieldPair pair) {
+			BeanUtilsFieldDescriptor fd1 = (BeanUtilsFieldDescriptor) pair.srcProp;
+			Class<?> srcType = fd1.pd.getPropertyType();
+			BeanUtilsFieldDescriptor desc = (BeanUtilsFieldDescriptor) pair.destProp;
+			Class<?> type = desc.pd.getPropertyType();
+			
+			if (Number.class.isAssignableFrom(srcType) || isNumberPrimitive(srcType)) {
+				if (Boolean.class.equals(type) || Boolean.TYPE.equals(type)) {			
+					return type;
+				} else if (Date.class.equals(type)) {
+					if (Long.class.equals(srcType) || Long.TYPE.equals(srcType)) {
+					} else {
+						return type;
+					}
+				}
+			} else if (Date.class.isAssignableFrom(srcType)) {
+				if (Boolean.class.equals(type) || Boolean.TYPE.equals(type)) {			
+					return type;
+				} else if (Integer.class.equals(type) || Integer.TYPE.equals(type)) {			
+					return type;
+				} else if (Double.class.equals(type) || Double.TYPE.equals(type)) {			
+					return type;
+				} else if (type.isEnum()) {			
+					return type;
+				}
+			} else if (srcType.isEnum()) {
+				if (type.isEnum()) {
+				} else if (String.class.equals(type)) {
+				} else {
+					return type;
+				}
+			} else if (Collection.class.isAssignableFrom(srcType)) {
+				if (Collection.class.isAssignableFrom(type) || type.isArray()) {
+				} else {
+					return type;
+				}
+			} else if (srcType.isArray()) {
+				if (type.isArray() || Collection.class.isAssignableFrom(type)) {
+				} else {
+					return type;
+				}
+			}
+			return null;
+		}
+		private boolean isNumberPrimitive(Class<?> srcType) {
+			if (Integer.TYPE.equals(srcType) ||
+					Long.TYPE.equals(srcType) ||
+					Double.TYPE.equals(srcType) ||
+					Float.TYPE.equals(srcType) ||
+					Short.TYPE.equals(srcType)) {
+				return true;
+			}
+			return false;
+		}
+		
+		
+		private ZClassPlan buildClassPlan(Object srcObj, Object destObj, Class<?> srcClass, Class<?> destClass, List<FieldPair> fieldPairs) throws Exception {
+			logger.log("BUILDPLAN!");
+			if (srcObj == null) {
+				String error = String.format("buildClassPlan. srcObj is NULL");
+				throw new FieldCopyException(error);
+			}
+			//destObj can be null
+			
+			ZClassPlan classPlan = new ZClassPlan();
+			classPlan.srcClass = srcClass;
+			classPlan.destClass = destClass;
+
+			for(FieldPair pair: fieldPairs) {
+				final FieldDescriptor origDescriptor = pair.srcProp;
+				final String name = origDescriptor.getName();
+				
+				//check for readability and writability
+				if (destObj != null) {
+					if (! propertyUtils.isReadable(srcObj, name)) {
+						String error = String.format("Source Field '%s' is not readable", name);
+						throw new FieldCopyException(error);
+					}
+					if (!propertyUtils.isWriteable(destObj, pair.destFieldName)) {
+						String error = String.format("Destination Field '%s' is not writeable", name);
+						throw new FieldCopyException(error);
+					}
+				}				
+				
+				
+	            
+	            //TODO add check for is-readable and writer later
+	            fillInDestPropIfNeeded(pair, destClass);
+
+	            ZFieldPlan fieldPlan = new ZFieldPlan();
+	            fieldPlan.srcFd = origDescriptor;
+	            fieldPlan.destFd = pair.destProp;
+	            
+	            BeanUtilsFieldDescriptor fd1 = (BeanUtilsFieldDescriptor) pair.srcProp;
+	            Class<?> srcType = fd1.pd.getPropertyType();
+
+	            if (beanDetectorSvc.isBeanClass(srcType)) {
+	            	if (destObj == null) {
+	            		fieldPlan.lazySubPlanFlag = true; //do later if this field is ever non-null
+	            	} else {
+	            		//recursively generate plan
+	            	}
+	            } else {
+        			//handle list
+        			//handle array
+        			//handle list to array, and viceversa
+        			validateIsAllowed(pair);
+	            	//add converter if one matches
+	            }
+			}
+			
+			return classPlan;
+		}
+		
 		
 		private void fillInDestPropIfNeeded(FieldPair pair, Class<? extends Object> class2) {
 			if (pair.destProp != null) {
