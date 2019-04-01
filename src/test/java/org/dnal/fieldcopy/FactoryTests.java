034014ebc064b3b8c5ab4696cb28242e26c5ee60
ianrae <ianrae88@gmail.com>
2019-04-01 11:25:02 -0400

    ok

--- src/test/java/org/dnal/fieldcopy/FactoryTests.java
+++ src/test/java/org/dnal/fieldcopy/FactoryTests.java
@@ -1,10 +1,10 @@
 package org.dnal.fieldcopy;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
 
 import org.dnal.fieldcopy.core.DefaultFieldFilter;
 import org.dnal.fieldcopy.core.FieldCopyService;
-import org.dnal.fieldcopy.core.FieldFilter;
 import org.dnal.fieldcopy.core.FieldRegistry;
 import org.dnal.fieldcopy.log.SimpleLogger;
 import org.dnal.fieldcopy.service.beanutils.BUCopyService;
@@ -13,6 +13,17 @@ import org.junit.Test;
 
 public class FactoryTests extends BaseTest {
 	
+	/**
+	 * A factory that creates FieldCopier objects.
+	 * FieldCopier objects are single-use objects.
+	 * Use one to do a single copy.
+	 * 
+	 * The copier factory caches details of the classes
+	 * that it has copied.
+	 * 
+	 * @author Ian Rae
+	 *
+	 */
 	public interface CopierFactory {
 		FieldCopier createCopier();
 	}
@@ -34,7 +45,7 @@ public class FactoryTests extends BaseTest {
 		FieldCopyService createService();
 	}
 	
-	public static class SvcF implements ServiceFactory {
+	public static class BUServiceFactory implements ServiceFactory {
 		private SimpleLogger logger;
 
 		@Override
@@ -55,17 +66,30 @@ public class FactoryTests extends BaseTest {
 			this.svcFactory = factory;
 		}
 		
+		/**
+		 * Optional method. Use this to set a custom factory.
+		 * 
+		 * @param factory
+		 */
 		public static synchronized void setSingleton(FieldCopyFactory factory) {
 			theSingleton = factory;
 		}
 		
+		/**
+		 * Create a copier factory in a thread-safe way.
+		 * @return new instance of a copier factory
+		 */
 		public static synchronized CopierFactory createFactory() {
 			if (theSingleton == null) {
-				theSingleton = new FieldCopyFactory(new SvcF());
+				theSingleton = new FieldCopyFactory(new BUServiceFactory());
 			}
 			return theSingleton.createF();
 		}
 
+		/**
+		 * Create a new instance of a copier factory, which contains
+		 * a new instance of a field copy service.
+		 */
 		private CopierFactory createF() {
 			FieldCopyService copySvc = svcFactory.createService();
 			CopierFactoryImpl dcf = new CopierFactoryImpl(copySvc);
@@ -76,17 +100,17 @@ public class FactoryTests extends BaseTest {
 	@Test
 	public void test() {
 		CopierFactory fact1 = FieldCopyFactory.createFactory();
-		FieldCopier fc1 = fact1.createCopier();
-		FieldCopier fc2 = fact1.createCopier();
+		FieldCopier fc1a = fact1.createCopier();
+		FieldCopier fc1b = fact1.createCopier();
 		
-		assertNotSame(fc1, fc2);
-		assertSame(fc1.getCopyService(), fc2.getCopyService());
+		assertNotSame(fc1a, fc1b);
+		assertSame(fc1a.getCopyService(), fc1b.getCopyService());
 		
 		CopierFactory fact2 = FieldCopyFactory.createFactory();
-		FieldCopier fc3 = fact2.createCopier();
+		FieldCopier fc2a = fact2.createCopier();
 		
 		assertNotSame(fact1, fact2);
-		assertSame(fc1.getCopyService(), fc3.getCopyService());
+		assertNotSame(fc1a.getCopyService(), fc2a.getCopyService());
 	}
 	
 }
