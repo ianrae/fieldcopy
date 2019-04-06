7a3308b807f2d4675af6af5fc5cf778586efd3b8
ianrae <ianrae88@gmail.com>
2019-04-06 06:56:15 -0400

    ok

--- src/test/java/org/dnal/fieldcopy/proploader/PropLoaderTests.java
+++ src/test/java/org/dnal/fieldcopy/proploader/PropLoaderTests.java
@@ -3,13 +3,10 @@ package org.dnal.fieldcopy.proploader;
 import static org.junit.Assert.assertEquals;
 
 import java.beans.PropertyDescriptor;
-import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.beanutils.BeanUtilsBean;
 import org.apache.commons.beanutils.ConvertUtils;
@@ -33,6 +30,7 @@ import org.dnal.fieldcopy.log.SimpleConsoleLogger;
 import org.dnal.fieldcopy.log.SimpleLogger;
 import org.dnal.fieldcopy.metrics.CopyMetrics;
 import org.dnal.fieldcopy.service.beanutils.BUBeanDetectorService;
+import org.dnal.fieldcopy.service.beanutils.BUHelperService;
 import org.dnal.fieldcopy.service.beanutils.BeanUtilsFieldDescriptor;
 import org.junit.Test;
 
@@ -63,6 +61,7 @@ public class PropLoaderTests extends BaseTest {
 		protected PropertyUtilsBean propertyUtils;
 		protected FieldFilter fieldFilter;
 		protected BUBeanDetectorService beanDetectorSvc;
+		protected BUHelperService helperSvc;
 
 		public PropLoaderService(SimpleLogger logger, FieldRegistry registry, FieldFilter fieldFilter) {
 			this.logger = logger;
@@ -71,14 +70,8 @@ public class PropLoaderTests extends BaseTest {
 			this.propertyUtils =  new PropertyUtilsBean();
 			this.fieldFilter = fieldFilter;
 			this.beanDetectorSvc = new BUBeanDetectorService();
-
-			//customize Date converter.  There is no good defaut for date conversions
-			//so well just do yyyy-MM-dd
-			DateConverter dateConverter = new DateConverter();
-			dateConverter.setPattern("yyyy-MM-dd");
-			ConvertUtils.register(dateConverter, Date.class);
+			this.helperSvc = new BUHelperService(logger);
 		}
-
 
 		@Override
 		public List<FieldPair> buildAutoCopyPairs(TargetPair targetPair) {
@@ -123,67 +116,9 @@ public class PropLoaderTests extends BaseTest {
 			return fieldPairs;
 		}
 
-		protected void validateIsAllowed(FieldPair pair) throws NoSuchMethodException, InstantiationException, IllegalAccessException {
-			Class<?> destClass = isNotAllowed(pair);
-			if (destClass != null) {
-				BeanUtilsFieldDescriptor fd1 = (BeanUtilsFieldDescriptor) pair.srcProp;
-				String err = String.format("Not allowed to copy %s to %s", fd1.pd.getPropertyType().getName(), destClass.getName());
-				throw new FieldCopyException(err);
-			}
-		}
-
-		/**
-		 * Additional rules that we want to enforce.
-		 * For example, not allowed to copy an int to a boolean.
-		 * 
-		 * @return null if allowed, else class that we are NOT allowed to copy to
-		 */
-		private Class<?> isNotAllowed(FieldPair pair) {
-			Class<?> type = pair.getDestClass();
-
-			if (Number.class.isAssignableFrom(type) || isNumberPrimitive(type)) {
-				return null;
-			} else if (Date.class.isAssignableFrom(type)) {
-				return null;
-			} else if (type.isEnum()) {
-				return null;
-			} else if (Collection.class.isAssignableFrom(type)) {
-				return null; //todo: do we support lists?
-			} else if (type.isArray()) {
-				return null;
-			} else {
-				return type; //not allowed
-			}
-		}
-		private boolean isNumberPrimitive(Class<?> srcType) {
-			if (Integer.TYPE.equals(srcType) ||
-					Long.TYPE.equals(srcType) ||
-					Double.TYPE.equals(srcType) ||
-					Float.TYPE.equals(srcType) ||
-					Short.TYPE.equals(srcType)) {
-				return true;
-			}
-			return false;
-		}
-
 		@Override
 		public void dumpFields(Object sourceObj) {
-			try {
-				Map<String,String> map = beanUtil.describe(sourceObj);
-				for(String fieldName: map.keySet()) {
-					String val = map.get(fieldName);
-					logger.log("%s = %s", fieldName, val);
-				}
-			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InvocationTargetException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (NoSuchMethodException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			helperSvc.dumpFields(sourceObj);
 		}
 
 		@Override
@@ -198,45 +133,11 @@ public class PropLoaderTests extends BaseTest {
 
 		@Override
 		public String generateExecutionPlanCacheKey(CopySpec spec) {
-			//NOTE. the following key will not work if you have multiple conversions of the
-			//same pair of source,destObj but with different fields, mappings, and converters.
-			//If that is the case, you MUST key cacheKey and provide a unique value.
-
-			//if source or destObj are null we will catch it during copy
-			String class1Name = spec.sourceObj == null ? "" : spec.sourceObj.getClass().getName();
-			String class2Name = spec.destObj == null ? "" : spec.destObj.getClass().getName();
-			return String.format("%s--%s", class1Name, class2Name);
-		}
-
-		protected Object getLoggableString(Object value) {
-			if (value == null || ! logger.isEnabled()) {
-				return null;
-			}
-
-			if (value.getClass().isArray()) {
-				int n = Array.getLength(value);
-				String s = String.format("array(len=%d): ", n);
-				for(int i = 0; i < n; i++) {
-					Object el = Array.get(value, i);
-					s += String.format("%s ", el.toString());
-					if (i >= 2) {
-						s += "...";
-						break;
-					}
-				}
-				return s;
-			} else {
-				String s = value.toString();
-				if (s.length() > 100) {
-					s = s.substring(0, 100);
-				}
-				return s;
-			}
+			return helperSvc.generateExecutionPlanCacheKey(spec);
 		}
 
 		@Override
 		public void copyFields(CopySpec copySpec) {
-			Object srcObj = copySpec.sourceObj;
 			Object destObj = copySpec.destObj;
 
 			for(FieldPair pair: copySpec.fieldPairs) {
@@ -304,23 +205,10 @@ public class PropLoaderTests extends BaseTest {
 
 		@Override
 		public <T> T copyFields(CopySpec copySpec, Class<T> destClass) {
-			T destObj = (T) createDestObject(destClass);
+			T destObj = (T) helperSvc.createObject(destClass);
 			copySpec.destObj = destObj;
 			copyFields(copySpec);
 			return destObj;
-		}
-
-		@SuppressWarnings("unchecked")
-		private <T> T createDestObject(Class<T> destClass) {
-			T obj = null;
-			try {
-				obj = (T) destClass.newInstance();
-			} catch (InstantiationException e) {
-				throw new FieldCopyException(e.getMessage());
-			} catch (IllegalAccessException e) {
-				throw new FieldCopyException(e.getMessage());
-			}
-			return obj;
 		}
 
 		@Override
